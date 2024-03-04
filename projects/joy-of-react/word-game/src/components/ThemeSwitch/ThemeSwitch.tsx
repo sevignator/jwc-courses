24c3c815f36997e362e03ecf76edc270a3472d21
Mikaël Sévigny <sevignator@hotmail.com>
2024-03-04 09:23:38 -0500

    Add ARIA attributes to theme switch

--- projects/joy-of-react/word-game/src/components/ThemeSwitch/ThemeSwitch.tsx
+++ projects/joy-of-react/word-game/src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,20 +1,23 @@
+import React from 'react';
+import { Moon, Sun } from 'lucide-react';
 import VisuallyHidden from '@components/VisuallyHidden';
 import useThemeContext from '@hooks/useThemeContext';
 import styles from './ThemeSwitch.module.css';
 
-import { Moon, Sun } from 'lucide-react';
-
 function ThemeSwitch() {
   const { theme, switchTheme } = useThemeContext();
+  const inputId = `switch-${React.useId()}`;
 
   return (
-    <label className={styles.wrapper} htmlFor='frequency'>
-      <VisuallyHidden>Switch theme</VisuallyHidden>
+    <label id={inputId} className={styles.wrapper} htmlFor='dark-mode'>
+      <VisuallyHidden>Toggle dark mode</VisuallyHidden>
       <div className={styles.switch}>
         <input
-          id='frequency'
+          id='dark-mode'
           className={styles.checkbox}
           type='checkbox'
+          role='switch'
+          aria-labelledby={inputId}
           checked={theme === 'dark'}
           onChange={(e) => switchTheme(e.target.checked ? 'dark' : 'light')}
         />
