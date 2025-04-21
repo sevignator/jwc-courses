30319c941a59f4e0dd480646226d4deb587916fe
Mikaël Sévigny <sevignator@hotmail.com>
2025-04-20 20:05:44 -0400

    Add animations to DivisionGroupsDemo

--- joy-of-react/project-blog/src/components/BlogContent/BlogContent.jsx
+++ joy-of-react/project-blog/src/components/BlogContent/BlogContent.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import CodeSnippet from "@/components/CodeSnippet";
+import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
 
 export default function BlogContent({ mdxContent }) {
-  return <MDXRemote source={mdxContent} />;
+  return (
+    <MDXRemote
+      source={mdxContent}
+      components={{
+        DivisionGroupsDemo,
+        pre: CodeSnippet,
+      }}
+    />
+  );
 }
--- joy-of-react/project-blog/src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ joy-of-react/project-blog/src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -1,30 +1,31 @@
-'use client';
-import React from 'react';
-import clsx from 'clsx';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { motion, LayoutGroup } from "framer-motion";
 
-import { range } from '@/utils';
-import Card from '@/components/Card';
-import SliderControl from '@/components/SliderControl';
+import { range } from "@/utils";
+import Card from "@/components/Card";
+import SliderControl from "@/components/SliderControl";
 
-import Equation from './Equation';
-import styles from './DivisionGroupsDemo.module.css';
+import Equation from "./Equation";
+import styles from "./DivisionGroupsDemo.module.css";
 
 function DivisionGroupsDemo({
   numOfItems = 12,
   initialNumOfGroups = 1,
   includeRemainderArea,
 }) {
-  const [numOfGroups, setNumOfGroups] = React.useState(
-    initialNumOfGroups
-  );
+  const [numOfGroups, setNumOfGroups] = React.useState(initialNumOfGroups);
 
-  const numOfItemsPerGroup = Math.floor(
-    numOfItems / numOfGroups
-  );
+  const numOfItemsPerGroup = Math.floor(numOfItems / numOfGroups);
 
-  const remainder = includeRemainderArea
-    ? numOfItems % numOfGroups
-    : null;
+  const remainder = includeRemainderArea ? numOfItems % numOfGroups : null;
+
+  const SPRING_TRANSITION = {
+    type: "spring",
+    stiffness: 400,
+    damping: 40,
+  };
 
   // When we're splitting into 1-3 groups, display side-by-side
   // columns. When we get to 4, it should switch to a 2x2 grid.
@@ -34,66 +35,73 @@ function DivisionGroupsDemo({
           gridTemplateColumns: `repeat(${numOfGroups}, 1fr)`,
         }
       : {
-          gridTemplateColumns: '1fr 1fr',
-          gridTemplateRows: '1fr 1fr',
+          gridTemplateColumns: "1fr 1fr",
+          gridTemplateRows: "1fr 1fr",
         };
 
   return (
-    <Card as="section" className={styles.wrapper}>
-      <header className={styles.header}>
-        <SliderControl
-          label="Number of Groups"
-          className={styles.slider}
-          step={1}
-          min={1}
-          max={4}
-          value={numOfGroups}
-          onChange={(ev) =>
-            setNumOfGroups(Number(ev.target.value))
-          }
+    <LayoutGroup>
+      <Card as="section" className={styles.wrapper}>
+        <header className={styles.header}>
+          <SliderControl
+            label="Number of Groups"
+            className={styles.slider}
+            step={1}
+            min={1}
+            max={4}
+            value={numOfGroups}
+            onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
+          />
+        </header>
+
+        <div className={styles.demoWrapper}>
+          <div className={clsx(styles.demoArea)} style={gridStructure}>
+            {range(numOfGroups).map((groupIndex) => (
+              <div key={groupIndex} className={styles.group}>
+                {range(numOfItemsPerGroup).map((index) => {
+                  const id = `item-${groupIndex * numOfItemsPerGroup + index}`;
+                  return (
+                    <motion.div
+                      key={id}
+                      layoutId={id}
+                      className={styles.item}
+                      transition={SPRING_TRANSITION}
+                    />
+                  );
+                })}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {includeRemainderArea && (
+          <div className={styles.remainderArea}>
+            <p className={styles.remainderHeading}>Remainder Area</p>
+
+            {range(remainder).map((index) => {
+              const id = `item-${
+                numOfItems - (numOfItems % numOfGroups) + index
+              }`;
+
+              return (
+                <motion.div
+                  key={id}
+                  layoutId={id}
+                  className={styles.item}
+                  transition={SPRING_TRANSITION}
+                />
+              );
+            })}
+          </div>
+        )}
+
+        <Equation
+          dividend={numOfItems}
+          divisor={numOfGroups}
+          remainder={remainder}
         />
-      </header>
-
-      <div className={styles.demoWrapper}>
-        <div
-          className={clsx(styles.demoArea)}
-          style={gridStructure}
-        >
-          {range(numOfGroups).map((groupIndex) => (
-            <div key={groupIndex} className={styles.group}>
-              {range(numOfItemsPerGroup).map((index) => {
-                return (
-                  <div
-                    key={index}
-                    className={styles.item}
-                  />
-                );
-              })}
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {includeRemainderArea && (
-        <div className={styles.remainderArea}>
-          <p className={styles.remainderHeading}>
-            Remainder Area
-          </p>
-
-          {range(remainder).map((index) => {
-            return (
-              <div key={index} className={styles.item} />
-            );
-          })}
-        </div>
-      )}
-
-      <Equation
-        dividend={numOfItems}
-        divisor={numOfGroups}
-        remainder={remainder}
-      />
-    </Card>
+      </Card>
+    </LayoutGroup>
   );
 }
 
